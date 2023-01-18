278b99d87762aa8eec65c000bd1930d9e35b72d9
Vansh Sukhija <12112021@nitkkr.ac.in>
2023-01-18 18:20:43 +0530

    initial commit

---
+++ landing/landing.js
@@ -0,0 +1,36 @@
+let teacher = document.getElementById('teacher');
+let student = document.getElementById('student');
+let visitor = document.getElementById('visitor');
+let club = document.getElementById('club');
+let body = document.getElementById('body');
+
+window.addEventListener('load', () => {
+    if(localStorage.role){
+        window.location.href = `${localStorage.role}/${localStorage.role}.html`;
+    }
+});
+
+teacher.addEventListener('click', () => {
+    localStorage.removeItem('role');
+    localStorage.setItem('role', 'teacher');
+    window.location.href = 'teacher/teacher.html';
+});
+
+student.addEventListener('click', () => {
+    localStorage.removeItem('role');
+    localStorage.setItem('role', 'student');
+    window.location.href = 'student/student.html';
+});
+
+visitor.addEventListener('click', () => {
+    localStorage.removeItem('role');
+    localStorage.setItem('role', 'visitor');
+    window.location.href = 'visitor/visitor.html';
+});
+
+club.addEventListener('click', () => {
+    localStorage.removeItem('role');
+    localStorage.setItem('role', 'club');
+    window.location.href = 'club/club.html';
+});
+
---
+++ landing/student/student.js
@@ -0,0 +1,88 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+let locations;
+let loclist = document.getElementById('loclist');
+console.log(loclist.childNodes);
+
+function initMap() {
+	const map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 16,
+		center: { lat: 29.946076, lng: 76.817682 },
+		mapTypeId: 'satellite'
+	});
+
+	const image = {
+		url: "marker.png",
+		// This marker is 20 pixels wide by 32 pixels high.
+		size: new google.maps.Size(20, 32),
+		// The origin for this image is (0, 0).
+		origin: new google.maps.Point(0, 0),
+		// The anchor for this image is the base of the flagpole at (0, 32).
+		anchor: new google.maps.Point(0, 32),
+	};
+	// Shapes define the clickable region of the icon. The type defines an HTML
+	// <area> element 'poly' which traces out a polygon as a series of X,Y points.
+	// The final coordinate closes the poly by connecting to the first coordinate.
+	const shape = {
+		coords: [1, 1, 1, 20, 18, 20, 18, 1],
+		type: "poly",
+	};
+
+	fetch('locations.json')
+	.then(response => response.json())
+	.then(data => {
+		data.places.forEach(element => {
+			loclist.innerHTML += `<li><a href="#" onclick="onClickHandler(this)">${element.name}</a></li>`;
+
+			const marker = new google.maps.Marker({
+				position: { lat: element.latitude, lng: element.longitude },
+				map,
+				icon: image,
+				shape: shape,
+				title: element.name,
+				animation: google.maps.Animation.DROP
+			});
+	
+			marker.addListener("click", (e) => {
+				const contentString =
+					'<div class="info-window-content">' +
+					'<h2>' + element.name + '</h2>' +
+					'<img src="https://www.mystudyindia.com/storage/colleges/medias/aTSlBx_1618989003.webp" style="width: 100%"/>' +
+					'</div>';
+			
+				infoWindow.setContent(contentString);
+				infoWindow.open({
+					anchor: marker,
+					map,
+					shouldFocus: false,
+				});
+			});
+		});
+	});
+
+	map.setOptions({minZoom: 15});
+
+	infoWindow = new google.maps.InfoWindow({
+		content: ""
+	});
+}
+
+function onClickHandler(element){
+	let str = element.innerHTML;
+	let obj;
+
+	fetch('locations.json')
+	.then(response => response.json())
+	.then(data => {
+		obj = data.places.find(el => el.name == str);
+		localStorage.setItem('targetLat', obj.latitude);
+		localStorage.setItem('targetLng', obj.longitude);
+		window.location.href = 'directions/directions.html'
+	});
+}
+
+// window.initMap = initMap;
+

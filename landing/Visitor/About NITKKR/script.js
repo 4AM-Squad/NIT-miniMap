7468e103f4bbd61e62fdbda18c6e43809ca3239b
Tushar <12112009@nitkkr,ac,in>
2023-01-28 00:30:45 +0530

    visitor added

---
+++ landing/Visitor/About NITKKR/script.js
@@ -0,0 +1,24 @@
+let navlinks = document.querySelectorAll("header nav a");
+
+
+for (const link of navlinks) {
+
+  link.addEventListener("click", smoothScroll);
+
+}
+
+
+function smoothScroll(event) {
+
+  event.preventDefault();
+
+  const href = this.getAttribute("href");
+
+
+  document.querySelector(href).scrollIntoView({
+
+    behavior: "smooth"
+
+  });
+
+}

2ef0520f4e5cdfba0ffda960bcf48fa019d05432
Tushar <12112009@nitkkr,ac,in>
2023-01-28 10:06:54 +0530

    removed direction bugs

--- landing/teacher/directions/directions.js
+++ landing/teacher/directions/directions.js
@@ -1,7 +1,7 @@
 
 // let watchID = navigator.geolocation.watchPosition(handleGetCurrentPosition, handleGetCurrentPositionError, {enableHighAccuracy:true, maximumAge: 0, timeout: 5000});
 
-let latt = 29.94798919310724, lngg = 76.81531926262228;
+let latt = 29.947001, lngg = 76.816805;
 
 // window.onload(navigator.geolocation.watchPosition(
 //     function (position) {
